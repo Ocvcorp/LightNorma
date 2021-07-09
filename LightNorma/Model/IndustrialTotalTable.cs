0f351ce6ea8df451a78693b2100805dc470b9054
Alexey <61292219+Ocvcorp@users.noreply.github.com>
2021-07-09 21:37:21 +0300

    Добавлен первый класс модели

---
+++ LightNorma/Model/IndustrialTotalTable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LightNorma.Model
+{
+    public class IndustrialTotalTable
+    {
+        public string EyeAccuracy { get; set; }
+        public float MinObjectSize0 { get; set; }
+        public float MinObjectSizeN { get; set; }
+        public char EyeHighLevel { get; set; }
+        public List<char> EyeLowLevel { get; set; }
+
+    }
+}
